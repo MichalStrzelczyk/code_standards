d609640e600cae6e22a7bb8e2b48b9b39f0c871f
Michał Strzelczyk <kontakt@digitalfarm.pl>
2018-05-13 23:12:05 +0200

    Add more methods to class

--- example.php
+++ example.php
@@ -19,6 +19,7 @@ class MyClass extends AbstractAdapter implements AdapterInterface, PriceInterfac
 
     const STATUS_ACCEPTED = 'accepted';
     const STATUS_FAILED = 'failed';
+    const TYPE = 'internal';
 
     /**
      * @var Repository
@@ -42,7 +43,7 @@ class MyClass extends AbstractAdapter implements AdapterInterface, PriceInterfac
      */
     public function __construct($type = null){
         $this->repository = $this->setRepository(Factory::create('\Domain\Repository'));
-        $this->images = Factory::create('\Doamin\Value\Image');
+        $this->image = Factory::create('\Domain\Value\Image');
         $this->type = $type;
     }
 
@@ -84,6 +85,8 @@ class MyClass extends AbstractAdapter implements AdapterInterface, PriceInterfac
      * @return bool
      */
     public function execute(string $foo, string $bar, int $amount): bool {
+
+        // More than two keys.
         $options = [
             'foo' => $foo,
             'bar' => $bar,
@@ -95,7 +98,7 @@ class MyClass extends AbstractAdapter implements AdapterInterface, PriceInterfac
     }
 
     /**
-     * This is testWithManyParameters method description
+     * This is doSomethingWithManyParameters method description
      *
      * @param string $foo
      * @param string $bar
@@ -112,7 +115,7 @@ class MyClass extends AbstractAdapter implements AdapterInterface, PriceInterfac
      *
      * @return bool
      */
-    public function testWithManyParameters(
+    public function doSomethingWithManyParameters(
         string $foo,
         string $bar,
         Config $config,
@@ -128,25 +131,95 @@ class MyClass extends AbstractAdapter implements AdapterInterface, PriceInterfac
             return $status;
         }
 
-        try{
-            switch ($type) {
-                case 'basic':
-                case 'default':
-                    $name = \Tools::generateCorrectName($bar); break;
-                default:
-                    $name = \Tools::generateCorrectName($name);
-            }
-
-            $personCount = $this->repository->countAllPerson($type);
-
-        }catch(\Exception $e){
-            
+        switch ($type) {
+            case 'basic':
+            case 'default':
+                $name = \Tools::generateCorrectName($bar); break;
+            default:
+                $name = \Tools::generateCorrectName($name);
         }
 
+        try{
+            $result = $this->repository->countSomething($name, $type);
+        }catch(\Domain\Repository\Exception\Count $e){
+            $this->logger->info('Repo can\'t count something important', ['name' => $name, 'type' => $type]);
+            $result = 0;
+        }
 
-
-        return $status;
+        return $amount < $result;
     }
 
+    /**
+     * Set something
+     *
+     * @param string $foo
+     * @param string $bar
+     * @param string $rad
+     * @param string $fad
+     *
+     * @return Image
+     */
+    public function setSomethingUsingChaining(string $foo, string $bar, string $rad, string $fad): Image {
+        $this->image
+            ->setFoo($foo)
+            ->setBar($bar)
+            ->setRad($rad)
+            ->setFad($fad)
+            ->setStatuses([self::STATUS_FAILED, self::STATUS_ACCEPTED])
+        ;
+
+        return $this->image;
+    }
+
+    /**
+     * Loop something
+     *
+     * @param int|null $limit
+     *
+     * @return array
+     */
+    public function loopSomething(int $limit = null){
+        if ($limit === null) {
+            $limit = $this->getRepository()->countLimit();
+        }
+
+        $result = [];
+        for ($i=0; $i<$limit; $i++) {
+            $result[$i] = $this->getRepository()->doSomething($i);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Method description
+     *
+     * Here's an example of how to format examples:
+     * <code>
+     *
+     * $this->>callSomething([
+     *  'test' => 'Test',
+     *  'test2' => 'Test2'
+     *  'test3' => 'Test3'
+     * ]);
+     *
+     * </code>
+     *
+     * Here is an example for non-php example or sample:
+     * <samp>
+     *
+     *   pear install net_sample
+     *
+     * </samp>
+     *
+     * @param array $options
+     *
+     * @return null|string
+     */
+    public function callSomething(array $options): ?string {
+        $options['type'] = self::TYPE;
+
+        return parent::callSomething($options);
+    }
 
 }
